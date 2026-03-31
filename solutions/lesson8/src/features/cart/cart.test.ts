0d50487e236aca8328240687dac35d2213cbb05c
Heiddi187 <heiddi187@gmail.com>
2026-03-31 11:47:04 +0000

    add test - Add product to set quantity 1

--- solutions/lesson8/src/features/cart/cart.test.ts
+++ solutions/lesson8/src/features/cart/cart.test.ts
@@ -1,5 +1,19 @@
 import { test, expect } from 'vitest'
+import type { Product } from '../products'
+import { renderHook, act } from '@testing-library/react'
+import { useCartStore } from '@/shared/store/appStore'
 
 test('1 + 1 equals 2', () => {
   expect(1 + 1).toBe(2)
+})
+
+const mockProduct: Product = { id: '1', name: 'test', price: 10};
+
+test('Add product to set quantity 1', () => {
+  const { result } = renderHook(() => useCartStore());
+  act(() => {
+    result.current.addToCart(mockProduct);
+  });
+
+  expect(result.current.items[0].quantity).toBe(1);
 })
